cb2e533eb40c508ad6a4a02a9ee0bf09adb0271a
Jeisson Prieto <japrietov@unal.edu.c>
2015-10-16 20:47:36 -0500

    User Update.

---
+++ grails-app/domain/CLASSES/User.groovy
@@ -0,0 +1,14 @@
+package CLASSES
+
+class User {
+    String name
+    String lastName
+    int id
+    String email
+    String nickname
+    String password
+    Object picture
+    static constraints = {
+
+    }
+}
