dca0afe7cfb004be17f4bef700a450e34747836b
phelipedotjc <phelipe.jc@gmail.com>
2015-10-19 16:30:50 -0500

    Se cambio el atributo points del StandardUser por una relacion con una nueva clase RedeemablePoints, para que el usuario pueda guardar los puntos que tiene acumulados con cada empresa cliente

--- grails-app/domain/CLASSES/StandardUser.groovy
+++ grails-app/domain/CLASSES/StandardUser.groovy
@@ -3,11 +3,14 @@ package CLASSES
 class StandardUser extends User{
     String gender
     Date birthday
-    int points
     String telephone
+    static hasMany = [redeemblepoints : RedeemablePoints]
 
     static constraints = {
         gender(inList: ['male', 'female'])
         telephone(size: 7..10)
     }
 }
+
+
+
